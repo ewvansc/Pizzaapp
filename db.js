f5e9f03f69fe3f47cf41930647c736e941146e4d
Ethan <ewvansc@pointpark.edu>
2025-06-11 15:53:41 -0400

    rid of env

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-const Pool = require("pg").Pool;
- require('dotenv').config();
- 
- const pool = new Pool ({
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
- 
- }); 
- 
- pool.on('connect', ()=> {
-     console.log('connected to database');
- });
- 
- 
- module.exports = pool;
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "ewvansc",
+  password: "JHxGcnqQymjq9l1bLAxLP3f0lafGNf4Z",
+  host: "dpg-d14s6g95pdvs73but700-a",
+  database: "galacticpizza",
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.on("connect", () => {
+  console.log("Connected to database");
+});
+
+module.exports = pool;
